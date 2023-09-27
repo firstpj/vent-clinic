9f0feb31434862eca3be92ab18e66cadadd851aa
firstpj <114886262+firstpj@users.noreply.github.com>
2023-09-27 18:12:48 +0100

    Add animals table and data and queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals
+(  
+    id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(100) NOT NULL,
+    date_of_birth date not null,
+    escape_attempts INT not null,
+    neutered boolean not null,
+    weight_kg decimal not null
 );
