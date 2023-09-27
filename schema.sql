710cba0178bdf3f3edda0968d6260dd7913bff0f
firstpj <114886262+firstpj@users.noreply.github.com>
2023-09-27 23:15:50 +0100

    license

--- schema.sql
+++ schema.sql
@@ -2,10 +2,10 @@
 
 CREATE TABLE animals
 (  
-    id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(100) NOT NULL,
-    date_of_birth date not null,
-    escape_attempts INT not null,
-    neutered boolean not null,
-    weight_kg decimal not null
+    id int,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts INT,
+    neutered boolean,
+    weight_kg decimal
 );
